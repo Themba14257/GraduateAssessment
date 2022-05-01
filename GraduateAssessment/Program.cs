8cfadf6c7713b11df42ac4e1a100af4d3ecf4543
Themba Richard Mahlangu <s219016879@mandela.ac.za>
2022-05-01 15:03:58 +0200

    Add project files.

---
+++ GraduateAssessment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace GraduateAssessment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+       
+
+    }
+}
